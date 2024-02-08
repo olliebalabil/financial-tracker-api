4b10d4764850d5afd9205e5dbde20831c118cb49
olliebalabil <oliverbalabil@gmail.com>
2024-02-08 13:03:19 +0000

    commit

--- obj/Debug/net8.0/TrackerApi.AssemblyInfo.cs
+++ obj/Debug/net8.0/TrackerApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TrackerApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b2624997a394e9143e6f1b5fb9825337e0f0c9bc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5ccb4b79548ac1bb250a24d061b895377d80bcd4")]
 [assembly: System.Reflection.AssemblyProductAttribute("TrackerApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TrackerApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
